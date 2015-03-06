2626bf326c1f30964707d01b0a35581b00469c4a
Joshua T Corbin <joshua@uber.com>
2015-03-06 00:40:43 -0800

    Test StructRW

--- test/struct_rw.js
+++ test/struct_rw.js
@@ -20,12 +20,46 @@
 
 'use strict';
 
-require('./atoms');
-require('./fixed_width_rw');
-require('./null_rw');
-require('./repeat_rw');
-require('./series');
-require('./skip');
-require('./string_rw');
-require('./struct_rw');
-require('./variable_buffer_rw');
+var structTest = require('./lib/struct_test');
+var test = require('tape');
+
+var atoms = require('../atoms');
+var StructRW = require('../struct');
+
+/*
+ * {
+ *     lat : DoubleBE
+ *     lng : DoubleBE
+ * }
+ */
+
+var anonLoc = StructRW({
+    lat: atoms.DoubleBE,
+    lng: atoms.DoubleBE
+});
+
+test('StructRW: anonLoc', structTest.cases(anonLoc, [
+    [{lat: 37.775497, lng: -122.417519},
+     [0x40, 0x42, 0xe3, 0x43, 0x7c, 0x56, 0x92, 0xb4,
+      0xc0, 0x5e, 0x9a, 0xb8, 0xa1, 0x9c, 0x9d, 0x5a]]
+]));
+
+function Loc(lat, lng) {
+    if (!(this instanceof Loc)) {
+        return new Loc(lat, lng);
+    }
+    var self = this;
+    self.lat = lat || 0;
+    self.lng = lng || 0;
+}
+
+var consLoc = StructRW(Loc, {
+    lat: atoms.DoubleBE,
+    lng: atoms.DoubleBE
+});
+
+test('StructRW: consLoc', structTest.cases(consLoc, [
+    [Loc(37.775497, -122.417519),
+     [0x40, 0x42, 0xe3, 0x43, 0x7c, 0x56, 0x92, 0xb4,
+      0xc0, 0x5e, 0x9a, 0xb8, 0xa1, 0x9c, 0x9d, 0x5a]]
+]));
